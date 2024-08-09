dc398557945c6c87413c8d458a13c6a60dde16de
cosine2A <109309785+cosine2A@users.noreply.github.com>
2024-08-09 19:35:04 +0530

    Create mathfunction.java
    Program demonstrate the working of the inbuilt math function in Java.

---
+++ mathfunction.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+public class mathop{
+	public static void main(String[] args){
+		double x = 28;
+		double y = 30;
+		System.out.println("Number 1 --> "+ x);
+		System.out.println("Number 2 --> "+ y);
+		System.out.println("Maximum of The Two Numbers is "+ Math.max(x,y));
+		System.out.println("Square Root of Y is  --> "+ Math.sqrt(y));
+		System.out.println("Power of x to y--> "+Math.pow(x,y));
+		System.out.println("Log of X is  --> "+ Math.log(x));
+		System.out.println("Log of Y is  --> "+ Math.log(y));
+		System.out.println("Log 1p of X is  --> "+ Math.log1p(x));
+		System.out.println("Log 10 of X is  --> "+ Math.log10(x));
+		System.out.println("Log 10 of Y is  --> "+ Math.log(y));
+		System.out.println("Exponent of X is  --> "+ Math.exp(x));
+		System.out.println("Exponent of Y is  --> "+ Math.exp(y));
+
+	}
+}
